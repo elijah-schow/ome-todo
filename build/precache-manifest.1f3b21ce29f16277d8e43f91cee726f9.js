31f28c37888f22a6f4f887ee341c27d89a4e9d97
Elijah Schow <elijah.schow@gmail.com>
2020-04-26 22:57:31 -0500

    Run production build
    Commiting the production build to source contro is not ideal, but
    it is good enough for a short lived project.

---
+++ build/precache-manifest.1f3b21ce29f16277d8e43f91cee726f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc2c2b502c3185a71d7c6f50662e6c4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f6da0d8df1e678903c6",
+    "url": "/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "9591a1cfc1d538307007",
+    "url": "/static/css/main.8b799f93.chunk.css"
+  },
+  {
+    "revision": "4f6da0d8df1e678903c6",
+    "url": "/static/js/2.2892c151.chunk.js"
+  },
+  {
+    "revision": "232aae28f8ee03096b30a3e0fbca302d",
+    "url": "/static/js/2.2892c151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9591a1cfc1d538307007",
+    "url": "/static/js/main.4bd9ffaa.chunk.js"
+  },
+  {
+    "revision": "68c7b584218db6ccf14c",
+    "url": "/static/js/runtime-main.267b4cf3.js"
+  }
+]);
