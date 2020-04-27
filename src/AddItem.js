2a258c6b4e530f14aba05bf5ee0cdf3c8c637928
Elijah Schow <elijah.schow@gmail.com>
2020-04-26 21:51:34 -0500

    Hide the add form while not in use

--- src/AddItem.js
+++ src/AddItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { useForm } from 'react-hook-form';
 import { v4 as uuid } from 'uuid';
@@ -8,81 +8,97 @@ const AddItem = ({ action }) => {
 
     const { handleSubmit, register, reset, errors } = useForm();
 
+    const [open, setOpen] = useState(false);
+
+    const onOpen = () => {
+        setOpen(true);
+    };
+
     const onSubmit = (data) => {
         action({
             id: uuid(),
             ...data,
         });
         reset();
+        setOpen(false);
     };
 
-    return (
-        <form onSubmit={handleSubmit(onSubmit)} className="card card-body mb-4">
+    return open
+        ? (
+            <form onSubmit={handleSubmit(onSubmit)} className="card card-body mb-4">
 
-            {/* Name */}
-            <div className="form-group">
-                <label htmlFor="name">Name</label>
-                <input
-                    id="name"
-                    className={classnames(
-                        "form-control",
-                        {
-                            "is-invalid": errors.name
-                        })}
-                    name="name"
-                    ref={register({ required: true })}
-                />
-                {errors.name &&
-                    <div className="invalid-feedback d-block">
-                        Name is required
-                    </div>}
-            </div>
-
-            {/* Description */}
-            <div className="form-group">
-                <label htmlFor="description">Description</label>
-                <textarea
-                    id="description"
-                    className="form-control"
-                    name="description"
-                    ref={register}
-                />
-            </div>
-
-            {/* Dates */}
-            <div className="row">
-
-                {/* Due Date */}
-                <div className="form-group col-6">
-                    <label htmlFor="due">Due</label>
+                {/* Name */}
+                <div className="form-group">
+                    <label htmlFor="name">Name</label>
                     <input
-                        type="date"
-                        id="due"
+                        id="name"
+                        className={classnames(
+                            "form-control",
+                            {
+                                "is-invalid": errors.name
+                            })}
+                        name="name"
+                        autoFocus
+                        ref={register({ required: true })}
+                    />
+                    {errors.name &&
+                        <div className="invalid-feedback d-block">
+                            Name is required
+                        </div>}
+                </div>
+
+                {/* Description */}
+                <div className="form-group">
+                    <label htmlFor="description">Description</label>
+                    <textarea
+                        id="description"
                         className="form-control"
-                        name="due"
+                        name="description"
                         ref={register}
                     />
                 </div>
 
-                {/* Done Date */}
-                <div className="form-group col-6">
-                    <label htmlFor="done">Done</label>
-                    <input
-                        type="date"
-                        id="done"
-                        className="form-control"
-                        name="done"
-                        ref={register}
-                    />
+                {/* Dates */}
+                <div className="row">
+
+                    {/* Due Date */}
+                    <div className="form-group col-6">
+                        <label htmlFor="due">Due</label>
+                        <input
+                            type="date"
+                            id="due"
+                            className="form-control"
+                            name="due"
+                            ref={register}
+                        />
+                    </div>
+
+                    {/* Done Date */}
+                    <div className="form-group col-6">
+                        <label htmlFor="done">Done</label>
+                        <input
+                            type="date"
+                            id="done"
+                            className="form-control"
+                            name="done"
+                            ref={register}
+                        />
+                    </div>
+
                 </div>
 
-            </div>
+                {/* Submit */}
+                <button className="btn btn-primary btn-block">Add</button>
 
-            {/* Submit */}
-            <button className="btn btn-primary btn-block">Add</button>
-
-        </form>
-    );
+            </form>
+        )
+        : (
+            <button
+                type="button"
+                className="btn btn-primary btn-block mb-3"
+                onClick={onOpen}
+            >Add</button>
+        );
 };
 
 AddItem.propTypes = {
